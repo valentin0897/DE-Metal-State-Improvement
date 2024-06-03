381e42ff257bbf237788ad53f912f928175d71e0
Valentin <08valentin0897@gmail.com>
2024-06-03 22:00:21 +0400

    Fix format style

--- utils.py
+++ utils.py
@@ -5,12 +5,14 @@ import requests
 
 
 def load_random_picture():
-    unsplash_access_key = os.getenv("UNSPLASH_ACCESS_KEY")
+    unsplash_access_key = os.getenv('UNSPLASH_ACCESS_KEY')
     headers = {
         'Authorization': f'Client-ID {unsplash_access_key}'
     }
-    response = requests.get('https://api.unsplash.com/photos/random',
-                            headers=headers)
+    response = requests.get(
+        'https://api.unsplash.com/photos/random',
+        headers=headers
+    )
     response.raise_for_status()
     image_url = response.json().get('urls').get('regular')
     return image_url
@@ -22,30 +24,33 @@ def send_to_teams(image_url, quote):
         'Content-Type': 'application/json'
     }
     message = {
-        "@type": "MessageCard",
-        "@context": "http://schema.org/extensions",
-        "summary": "Daily Inspiration",
-        "sections": [{
-            "activityTitle": "Valentin Krivolutskii:",
-            "text": quote,
-            "images": [{
-                "image": image_url
+        '@type': 'MessageCard',
+        '@context': 'http://schema.org/extensions',
+        'summary': 'Daily Inspiration',
+        'sections': [{
+            'activityTitle': 'Valentin Krivolutskii:',
+            'text': quote,
+            'images': [{
+                'image': image_url
             }]
         }]
     }
-    response = requests.post(webhook_url,
-                             headers=headers, data=json.dumps(message))
+    response = requests.post(
+        webhook_url,
+        headers=headers,
+        data=json.dumps(message)
+    )
     return response.text
 
 
 def get_quote():
-    url = "https://shakespeare1.p.rapidapi.com/shakespeare/generate/lorem-ipsum"
+    url = 'https://shakespeare1.p.rapidapi.com/shakespeare/generate/lorem-ipsum'
 
     headers = {
-        "X-RapidAPI-Key": os.getenv("RAPIDAPI_KEY"),
-        "X-RapidAPI-Host": "shakespeare1.p.rapidapi.com"
+        'X-RapidAPI-Key': os.getenv('RAPIDAPI_KEY'),
+        'X-RapidAPI-Host': 'shakespeare1.p.rapidapi.com'
     }
 
     response = requests.get(url, headers=headers)
 
-    print(response.json()["contents"]["lorem-ipsum"])
+    print(response.json()['contents']['lorem-ipsum'])
